a3d5ea665e3228950c8b04c94fada5c9191423d8
bhiln <brhiln@gmail.com>
2015-02-08 17:59:52 -0500

    made elevation opacity .4

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ function plotElevation(results, status) {
         var pathOptions = {
             path: [elevationPath[i], elevationPath[i+1]],
             strokeColor: 'hsl(' + (50-(50-0)*((elevations[i].elevation-min)/(max-min))) + ',80%,50%)',
-            opacity: .8,
+            opacity: .4,
             strokeWeight: 15,
             map: map
         }
