8398227d8a58e48014362ee947124b9872d57faf
bhiln <brhiln@gmail.com>
2015-02-08 18:02:32 -0500

    increased size to 80

--- js/script.js
+++ js/script.js
@@ -219,7 +219,7 @@ function getHill(loc) {
 
     latslongs.push(new google.maps.LatLng(curLat, curLong));
 
-    for (var j = 0; j < 40; j++){
+    for (var j = 0; j < 80; j++){
         // Create a LocationElevationRequest object using the array's one value
         
         
