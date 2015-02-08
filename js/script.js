b8b2d19fae986ce5d0511738936533f7ac7b6e81
bhiln <brhiln@gmail.com>
2015-02-08 05:20:25 -0500

    back to plotting entire path

--- js/script.js
+++ js/script.js
@@ -130,7 +130,7 @@ function plotElevation(results, status) {
 
     // Display a polyline of the elevation path.
     var pathOptions = {
-        path: [elevationPath[0]],
+        path: elevationPath,
         strokeColor: '#FF0000',
         opacity: 1,
         strokeWeight: 10,
