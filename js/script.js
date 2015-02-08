bb6300314e2d2fef991770971da88bc6c617a044
bhiln <brhiln@gmail.com>
2015-02-08 18:15:37 -0500

    draw directions after path

--- js/script.js
+++ js/script.js
@@ -198,9 +198,9 @@ function calcRoute(start, end) {
     };
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(result);
             //for each (point in result.routes[0][)
             drawPath(result.routes[0].overview_path);
+            directionsDisplay.setDirections(result);
             //console.log(result);
             document.getElementById("location-start").textContent   = result.routes[0].legs[0].start_address;
             document.getElementById("location-end").textContent     = result.routes[0].legs[0].end_address;
