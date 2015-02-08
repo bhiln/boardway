60da4142c71a63cb38991139775f8ca0d3071ac4
bhiln <brhiln@gmail.com>
2015-02-08 05:32:58 -0500

    trying to make gradient line

--- js/script.js
+++ js/script.js
@@ -129,16 +129,17 @@ function plotElevation(results, status) {
     document.getElementById("elevation-min").textContent    = min;
 
     // Display a polyline of the elevation path.
-    var pathOptions = {
-        path: [elevationPath[0], elevationPath[1]],
-        strokeColor: '#FF0000',
-        opacity: 1,
-        strokeWeight: 10,
-        map: map
+    for (var i = 0; i < elavationPath.length; i+2){
+        var pathOptions = {
+            path: [elevationPath[i], elevationPath[i+1]],
+            strokeColor: hsl(10+(40-10)*((elevationPath[i].elevation-min)/(max-min)),80%,100%),
+            opacity: 0.4,
+            strokeWeight: 10,
+            map: map
+        }
+        console.log(pathOptions);
+        polyline = new google.maps.Polyline(pathOptions);
     }
-    console.log(pathOptions);
-    polyline = new google.maps.Polyline(pathOptions);
-
     // Extract the data from which to populate the chart.
     // Because the samples are equidistant, the 'Sample'
     // column here does double duty as distance along the
