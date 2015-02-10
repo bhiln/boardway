849590428b197ae1570c5519093b80711217382c
bhiln <brhiln@gmail.com>
2015-02-10 17:57:55 -0500

    try plotting only half at higher resolution

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ function drawPath(path) {
     // Create a PathElevationRequest object using this array.
     // Ask for 256 samples along that path.
     var pathRequest = {
-        'path': path,
+        'path': path.slice(0, path.lentgh/2),
         'samples': path.length
     }
 
