f92a85cc03856420bf3ed61e355b332469dd59cc
bhiln <brhiln@gmail.com>
2015-02-11 17:58:55 -0500

    attach it to my map object

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ if (navigator.geolocation) {
         icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png', new google.maps.Size(22,22), new google.maps.Point(0,18), new google.maps.Point(11,11)),
         shadow: null,
         zIndex: 999,
-        map: // your google.maps.Map object
+        map: map
     });
 
     myloc.setPosition(myLoc1);
