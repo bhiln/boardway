f12de222d2c3b6c14ada87eb041189f42b965936
bhiln <brhiln@gmail.com>
2015-02-11 18:03:15 -0500

    another try using code from the Google API

--- js/script.js
+++ js/script.js
@@ -66,15 +66,11 @@ if (navigator.geolocation) {
     }
     );
 
-    var myloc = new google.maps.Marker({
-        clickable: false,
-        icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png', new google.maps.Size(22,22), new google.maps.Point(0,18), new google.maps.Point(11,11)),
-        shadow: null,
-        zIndex: 999,
-        map: map
+    var marker = new google.maps.Marker({
+        position: myLoc1,
+        map: map,
+        title:"You are here!"
     });
-
-    myloc.setPosition(myLoc1);
 }
 
 function initialize() {
