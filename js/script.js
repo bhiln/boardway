076acc42de1994e00950a40e1eb1da68ed0f6f11
bhiln <brhiln@gmail.com>
2015-02-11 17:54:46 -0500

    try adding my location marker

--- js/script.js
+++ js/script.js
@@ -65,6 +65,19 @@ if (navigator.geolocation) {
         enableHighAccuracy: true
     }
     );
+
+    var myloc = new google.maps.Marker({
+        clickable: false,
+        icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+                                                        new google.maps.Size(22,22),
+                                                        new google.maps.Point(0,18),
+                                                        new google.maps.Point(11,11)),
+        shadow: null,
+        zIndex: 999,
+        map: // your google.maps.Map object
+    });
+
+    myloc.setPosition(myLoc1);
 }
 
 function initialize() {
