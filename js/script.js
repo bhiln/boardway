0cbf69f82995f0dd4fc87ae317ea3826e9c2bfa2
bhiln <brhiln@gmail.com>
2015-02-11 18:28:53 -0500

    take out bounce animation

--- js/script.js
+++ js/script.js
@@ -96,17 +96,24 @@ function initialize() {
         console.log("No web storage support...");
     }
 
-    var markerImage = new google.maps.MarkerImage("../img/bw_pin.png");
-    markerImage.size = new google.maps.Size(20, 32);
+    var markerImage = {
+        url: "../img/bw_pin.png",
+        // This marker is 20 pixels wide by 32 pixels tall.
+        size: new google.maps.Size(20, 32),
+        // The origin for this image is 0,0.
+        origin: new google.maps.Point(0,0),
+        // The anchor for this image is the base of the flagpole at 0,32.
+        anchor: new google.maps.Point(0, 32)
+    };
 
     var marker = new google.maps.Marker({
         position: myCurLoc,
         map: map,
         icon: markerImage,
-        animation: google.maps.Animation.DROP,
+        //animation: google.maps.Animation.DROP,
         title:"You are here!"
     });
-    google.maps.event.addListener(marker, 'click', toggleBounce);
+    //google.maps.event.addListener(marker, 'click', toggleBounce);
 
     getHill(myCurLoc);
 }
